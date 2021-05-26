6c91a2958c994de16c17fe56d33fc53ca7d0742b
Chiupam <65508083+chiupam@users.noreply.github.com>
2021-05-26 11:25:59 +0800

    Add files via upload

---
+++ serverless/deploy.sh
@@ -0,0 +1,23 @@
+#将需要的代码复制并cd到severless文件夹
+echo "开始拷贝必要文件"
+sudo cp main.py ./serverless
+sudo cp requirements.txt ./serverless
+sudo cp cookie.txt ./serverless
+cd ./serverless
+
+#安装云函数需要的依赖库到severless文件夹
+echo "开始安装所需模块"
+sudo -H pip install --upgrade setuptools >/dev/null
+sudo -H pip install -r ./requirements.txt -t ./
+
+#部署至腾讯云函数
+if [ -z "$TENCENT_SECRET_ID" ] || [ -z "$TENCENT_SECRET_KEY" ]; then
+  echo "部署至腾讯云需要填写TENCENT_SECRET_ID和TENCENT_SECRET_KEY两个secrets，跳过部署"
+else
+  sed -i "s/.\/log.txt/..\/..\/tmp\/log.txt/g" ./main.py
+  echo "开始安装腾讯ServerlessFramework"
+  sudo npm install -g serverless
+  echo "开始部署至腾讯云函数"
+  sls deploy --debug
+  exit 0
+fi
