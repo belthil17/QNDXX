057b7a146357fe83f93f351615037476b95fa2a0
Chiupam <65508083+chiupam@users.noreply.github.com>
2021-05-31 16:27:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests, json, time
 
 
-def run(Cookie):
+def erverWeek(Cookie): # 每周青年大学习
     url = 'http://home.yngqt.org.cn/qndxx/xuexi.ashx'
     headers = {
         "Host": "home.yngqt.org.cn",
@@ -27,7 +27,33 @@ def run(Cookie):
             else:
                 print(res)
         else:
-            print(r.text)
+            print(f'发生未知错误：\n{r.text}')
+    except Exception as e:
+        print(e)
+
+
+def everyDay(Cookie): # 每日签到领积分
+    url = "http://home.yngqt.org.cn/qndxx/user/qiandao.ashx"
+    headers = {
+        "Host": "home.yngqt.org.cn",
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-cn",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Origin": "http://home.yngqt.org.cn",
+        "Content-Length": "0",
+        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.4(0x1800042c) NetType/4G Language/zh_CN",
+        "Referer": "http://home.yngqt.org.cn/qndxx/user/guize.aspx",
+        "Cookie": Cookie
+    }
+    try:
+        r = requests.post(url, headers=headers)
+        if r.ok:
+            resp = r.json()
+            print(resp['message'])
+        else:
+            print(f'发生未知错误：\n{r.text}')
     except Exception as e:
         print(e)
 
@@ -35,7 +61,11 @@ def run(Cookie):
 def main(event, context):
     with open('./config.json', 'r', encoding='utf-8') as f:
         Cookie = json.load(f)['Cookie']
-    run(Cookie)
+    if time.localtime()[6] in [1,2,3]: # 周一、周二、周三各学习一遍
+        erverWeek(Cookie)
+        everyDay(Cookie)
+    else:
+        everyDay(Cookie)
 
 
 if __name__ == '__main__':
