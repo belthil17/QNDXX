dab1c5fe69386c554f4f3355e4b5ff1620f6334b
Chiupam <65508083+chiupam@users.noreply.github.com>
2021-05-31 17:09:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests, json, time
 
 
-def erverWeek(Cookie): # 每周青年大学习
+def everyWeek(Cookie): # 每周青年大学习
     url = 'http://home.yngqt.org.cn/qndxx/xuexi.ashx'
     headers = {
         "Host": "home.yngqt.org.cn",
@@ -19,17 +19,18 @@ def erverWeek(Cookie): # 每周青年大学习
         "txtid": time.localtime()[7] // 7 + 51
     }
     try:
-        r = requests.post(url, data=json.dumps(data), headers=headers)
+        r = requests.post(url, data=json.dumps(data), headers=headers, proxies={"http": None, "https": None})
+        r.encoding = r.apparent_encoding
         if r.ok:
             res = r.json()
-            if res['code'] == '100' or res['code'] == '102':
-                print(res['message'])
-            else:
-                print(res)
+            print(res['message'])
+            return True
         else:
-            print(f'发生未知错误：\n{r.text}')
+            print(f'每周阅读发生未知错误：\n{r.text}')
+            return False
     except Exception as e:
         print(e)
+        return False
 
 
 def everyDay(Cookie): # 每日签到领积分
@@ -48,24 +49,40 @@ def everyDay(Cookie): # 每日签到领积分
         "Cookie": Cookie
     }
     try:
-        r = requests.post(url, headers=headers)
+        r = requests.post(url, headers=headers, proxies={"http": None, "https": None})
+        r.encoding = r.apparent_encoding
         if r.ok:
             resp = r.json()
             print(resp['message'])
+            return True
         else:
-            print(f'发生未知错误：\n{r.text}')
+            print(f'每日签到发生未知错误：\n{r.text}')
+            return False
     except Exception as e:
         print(e)
+        return False
 
 
 def main(event, context):
     with open('./config.json', 'r', encoding='utf-8') as f:
         Cookie = json.load(f)['Cookie']
-    if time.localtime()[6] in [1,2,3]: # 周一、周二、周三各学习一遍
-        erverWeek(Cookie)
-        everyDay(Cookie)
+    if time.localtime()[6] in [0,1,2]: # 周一、周二、周三各学习一遍
+        i = 0
+        while everyWeek(Cookie):
+            i += 1
+            print(f'每周阅读：休眠 30 秒后，准备进行第{i}次重试……')
+            time.sleep(30)
+        i = 0
+        while everyDay(Cookie):
+            i += 1
+            print(f'每日签到：休眠 30 秒后，准备进行第{i}次重试……')
+            time.sleep(30)
     else:
-        everyDay(Cookie)
+        i = 0
+        while everyDay(Cookie):
+            i += 1
+            print(f'每日签到：休眠 30 秒后，准备进行第{i}次重试……')
+            time.sleep(30)
 
 
 if __name__ == '__main__':
